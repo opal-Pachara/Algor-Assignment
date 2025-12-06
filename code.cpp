69ed4249439f1c9c559fe6173b0d9ebff82f5263
opal-Pachara <s6404062610324@email.kmutnb.ac.th>
2025-12-06 10:41:27 +0700

    first commit

---
+++ code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void insertarr(int arr[],int n){
+    for(int i = 0;i < n;i++){
+        cin>>arr[i];
+    }
+}
+
+void printarr(int arr[],int n){
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+void sumtarget(int arr[],int n,int target){
+    for(int i = 0;i<n;i++){
+        for(int j = i+1;j<n;j++){
+            if(arr[i]+arr[j] == target){
+                cout<<arr[i]<<" "<<arr[j]<<" "<<endl;
+            }
+        }
+    }
+}
+
+
+int main(){
+    int n,target;
+    cin>>n;
+    int arr[n];
+
+    insertarr(arr,n);
+    // printarr(arr,n);
+    cin>>target;
+    sumtarget(arr,n,target);
+
+    return 0;
+}
