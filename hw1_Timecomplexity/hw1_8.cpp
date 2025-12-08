3e4f38f4a12e7023c6f44778a5cec8c5c7201866
opal-Pachara <s6404062610324@email.kmutnb.ac.th>
2025-12-08 18:49:41 +0700

    hw8

--- hw1_Timecomplexity/hw1_8.cpp
+++ hw1_Timecomplexity/hw1_8.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int maximum(int arr[],int n){
-    int max = arr[0];
-    for(int i = 0;i<n ;i++){
+void insertarr(int arr[],int n){
+    for(int i = 0 ; i < n; i++){
+        cin>>arr[i];
+    }
+}
+
+void printarr(int arr[],int n){
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int subsequebce(int arr[],int n){
+    int findmax = arr[0];
+    for(int i = 0;i<n;i++){
         for(int j = i;j<n;j++){
-            int findmax = 0;
-            for(int k = i;k<j;k++){
-                findmax += arr[k];
-            }
-            if(findmax > max){
-                max = findmax;
+            int sum = 0;
+            for(int k = i;k<=j;k++){
+                sum += arr[k];
+                // cout<<"loop(1)  "<<k<<": "<<sum<<"   findmax: "<<findmax<<endl;
+                if(sum > findmax){
+                    findmax = sum;
+                    // cout<<"loop  "<<k<<": "<<findmax<<endl;
+                }
             }
         }
     }
-    return max;
+    return findmax;
 }
 
-
 int main(){
-    int n = 5;
-    int arr[n] = {5,15,20,-30,40};
-    cout<<maximum(arr,n);
+    int n;
+    cin>>n;
+    int arr[n];
+    insertarr(arr,n);
+    printarr(arr,n);
+    cout<<subsequebce(arr,n);
 
     return 0;
 }
